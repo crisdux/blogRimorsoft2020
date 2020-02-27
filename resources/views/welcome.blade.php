4491a81d529ff7d51a09d6bfcf5980119840ad4a
cris.dux <crisfer.4217@gmail.com>
2020-02-27 19:16:24 -0400

    Category, Post, Tag models creados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    jfjlsdflbbbcccc
                 </div>
 
                 <div class="links">
