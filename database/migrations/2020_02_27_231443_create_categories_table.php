1cee3199d5c8b7c5dc8f86d74f6fe46cff3d5052
cris.dux <crisfer.4217@gmail.com>
2020-02-27 19:28:46 -0400

    se configuro la migracion Category

--- database/migrations/2020_02_27_231443_create_categories_table.php
+++ database/migrations/2020_02_27_231443_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name',64);
+            $table->string('slug',128)->unique();
+            $table->mediumText('body')->nullable();
             $table->timestamps();
         });
     }
